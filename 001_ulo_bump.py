2c71d779c9839e27f4e12d683fa31d493d7d99bc
giadarol <giovanni.iadarola@cern.ch>
2023-04-05 15:44:31 +0200

    We have an obstacle

--- 001_ulo_bump.py
+++ 001_ulo_bump.py
@@ -45,16 +45,44 @@ fig1.suptitle(
 
 import scipy.io as sio
 chamber_data = sio.loadmat('LHC_chm_ver.mat')
+chamber = xt.LimitPolygon(
+    x_vertices=chamber_data['Vx'][0],
+    y_vertices=chamber_data['Vy'][0])
+
+vx0 = chamber_data['Vx'][0].copy()
+vy0 = chamber_data['Vy'][0].copy()
+
+vx = vx0.copy()
+vy = vy0.copy()
+
+additional_points = [
+    [-7e-3, -10e-3],
+    [0e-3, -1.3e-3],
+    [1e-2, 1e-3]
+]
+
+vx = list(vx[:-15])
+vy = list(vy[:-15])
+
+for pp in additional_points:
+    vx.append(pp[0])
+    vy.append(pp[1])
+
+vx += list(vx0[-3:]) + [vx0[0]]
+vy += list(vy0[-3:]) + [vy0[0]]
+
+
 obstacle = xt.LimitPolygon(
     x_vertices=chamber_data['Vx'][0],
     y_vertices=chamber_data['Vy'][0])
+
 
 collider.lhcb1.track(bunch, ele_start=0, ele_stop=ulo_at)
 
 
 fig_ulo = plt.figure(2)
 ax_ulo = fig_ulo.add_subplot(111)
-ax_ulo.plot(obstacle.x_vertices, obstacle.y_vertices)
+ax_ulo.plot(obstacle.x_vertices, obstacle.y_vertices, '.-')
 ax_ulo.plot(bunch.x, bunch.y, '.', markersize=1)
 
 
